55664009ad65bfc48950dbed993eb22260b31664
absarrahman <raman040635@gmail.com>
2020-10-11 19:50:54 +0600

    Fix redirect mail issue
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- lib/screens/departments/departments.dart
+++ lib/screens/departments/departments.dart
@@ -139,19 +139,17 @@ class _DepartmentsPageState extends State<DepartmentsPage> {
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: InkWell(
+                  child: SelectableText(
+                    "${listShow[index].email}",
                     onTap: () => _launchEmail(listShow[index].email),
-                    child: SelectableText(
-                      "${listShow[index].email}",
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        fontSize: 20,
-                        color: isDark ? Colors.blue[400] : Colors.blue,
-                      ),
-                      maxLines: 2,
-                      textAlign: TextAlign.center,
-                      scrollPhysics: ClampingScrollPhysics(),
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      fontSize: 20,
+                      color: isDark ? Colors.blue[400] : Colors.blue,
                     ),
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    scrollPhysics: ClampingScrollPhysics(),
                   ),
                 ),
               ],
